c331c96154bba313c4d0d532ab2759126efaf32a
InventerBots <luke@ebarnfarm.com>
2017-10-13 17:14:30 -0500

    Update BasicTeleOpMode.java

--- BasicTeleOpMode.java
+++ BasicTeleOpMode.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * Created by Luke on 10/12/2017.
